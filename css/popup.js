9ea01fdda563d21157acfb28c6fff47af604641a
odoovo.com <odoovoapps@gmail.com>
2026-05-03 07:22:48 +0700

    www.onvodo.com discount 20% all modules odoo

--- css/popup.js
+++ css/popup.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function () {
     const waitBootstrap = setInterval(() => {
+
         if (typeof bootstrap === 'undefined') {
             return;
         }
@@ -7,36 +8,46 @@ window.addEventListener('load', function () {
         const modalElement =
             document.getElementById('migrationModal');
 
-      
+    
         if (!modalElement) {
             return;
         }
+
         const modal = new bootstrap.Modal(modalElement, {
             backdrop: true,
             keyboard: true
         });
 
         let reopenTimer = null;
+
         setTimeout(() => {
             modal.show();
         }, 1200);
-      
+        modalElement.addEventListener('hide.bs.modal', function () {
+
+            if (document.activeElement) {
+                document.activeElement.blur();
+            }
+
+        });
         modalElement.addEventListener('hidden.bs.modal', function () {
+
             clearTimeout(reopenTimer);
+
             reopenTimer = setTimeout(() => {
                 modal.show();
             }, 20000);
 
         });
-
-        document.querySelectorAll('[data-bs-toggle="dropdown"]')
+        document
+            .querySelectorAll('[data-bs-toggle="dropdown"]')
             .forEach(dropdown => {
 
                 if (!dropdown) {
                     return;
                 }
 
-                dropdown.addEventListener('click', function (e) {
+                dropdown.addEventListener('click', function () {
 
                     const target =
                         dropdown.nextElementSibling;
